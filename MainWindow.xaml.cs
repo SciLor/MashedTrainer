74e129c0b681cc139808a4e3735f1dcf6fd925aa
SciLor <bh44l@gmx.de>
2017-01-07 22:45:52 +0100

    First commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SciLors_Mashed_Trainer.Types;
+using System.Windows.Threading;
+using System.Diagnostics;
+
+namespace SciLors_Mashed_Trainer {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
+
+        public MainWindow() {
+            InitializeComponent();
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            timer.Start();
+
+            //Control baseGroup = (Control)FindName("grpP1");
+            //baseGroup.Tag = 0;
+
+        }
+        public void ReadPlayer(Player player) {
+            GetControl<Label>(player, "lbl", "Points").Content = String.Format("{0} / 8", player.Points);
+            GetControl<Slider>(player, "sld", "Points").Value = player.Points;
+
+            GetControl<TextBox>(player, "txt", "PositionX").Text = player.PosX + "";
+            GetControl<TextBox>(player, "txt", "PositionY").Text = player.PosY + "";
+            GetControl<TextBox>(player, "txt", "PositionZ").Text = player.PosZ + "";
+
+        }
+        public T GetControl<T>(Player player, string prefix, string suffix) {
+            return (T)FindName(prefix + "P" + ((int)player.Id + 1) + suffix);
+        }
+
+        private IntPtr hMashed = IntPtr.Zero;
+        private void timer_Tick(object sender, EventArgs e) {
+            Process[] proc = Process.GetProcessesByName("MFL");
+            if (proc.Length == 1) {
+                if (hMashed == IntPtr.Zero) {
+                    hMashed = BaseMemory.OpenProcess((uint)(
+                        BaseMemory.ProcessAccessType.PROCESS_VM_READ |
+                        BaseMemory.ProcessAccessType.PROCESS_VM_WRITE |
+                        BaseMemory.ProcessAccessType.PROCESS_VM_OPERATION)
+                        , 1, (uint)proc[0].Id);
+                } else {
+                    Player p1 = new Player(hMashed, Player.PlayerId.ONE);
+                    ReadPlayer(p1);
+                }
+            } else if (hMashed != IntPtr.Zero) {
+                    BaseMemory.CloseHandle(hMashed);
+                    hMashed = IntPtr.Zero;
+            }
+        }
+    }
+}
---
+++ Types/Player.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SciLors_Mashed_Trainer.Types {
+    public class Player : BaseMemory {
+        public enum PlayerId {
+            ONE = 0,
+            TWO = 1,
+            THREE = 2,
+            FOUR = 3
+        }
+
+        private const int BASE_ADDRESS = 0x8B06E0;
+        private const int BASE_ADDRESS_EXTENDER = 0x928;
+        private const int BASE_POINTS_ADDRESS = 0x8D8B40;
+
+        private const int PLAYER_POSITION_X = BASE_ADDRESS_EXTENDER + 0x30;
+        private const int PLAYER_POSITION_Y = BASE_ADDRESS_EXTENDER + 0x38;
+        private const int PLAYER_POSITION_Z = BASE_ADDRESS_EXTENDER + 0x34;
+
+        //Distances between each players options
+        private const int PLAYER_BASE_DISTANCE = 0xD04;
+        private const int PLAYER_WEAPON_DISTANCE = 0xB4;
+        private const int PLAYER_POINTS_DISTANCE = 0x4;
+
+        private int addressPoints;
+        public int Points {
+            get {
+                return ReadInt(addressPoints);
+            }
+            set {
+                Write(addressPoints, value);
+            }
+        }
+
+        private int addressBase;
+        public float PosX {
+            get {
+                return ReadFloat(addressBase + PLAYER_POSITION_X);
+            }
+            set {
+                Write(addressBase + PLAYER_POSITION_X, value);
+            }
+        }
+        public float PosY {
+            get {
+                return ReadFloat(addressBase + PLAYER_POSITION_Y);
+            }
+            set {
+                Write(addressBase + PLAYER_POSITION_Y, value);
+            }
+        }
+        public float PosZ {
+            get {
+                return ReadFloat(addressBase + PLAYER_POSITION_Z);
+            }
+            set {
+                Write(addressBase + PLAYER_POSITION_Z, value);
+            }
+        }
+        
+        public PlayerId Id { get; set; }
+        public Player(IntPtr hProcess, PlayerId id) : base(hProcess) {
+            Id = id;
+            addressPoints = BASE_POINTS_ADDRESS + PLAYER_POINTS_DISTANCE * (int)Id;
+            addressBase = BASE_ADDRESS + PLAYER_BASE_DISTANCE * (int)Id;
+        }
+    }
+}
