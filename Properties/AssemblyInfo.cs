81b967c0e164f622e54a1a6392f40440859a867b
SciLor <scilor@users.noreply.github.com>
2017-11-06 22:18:51 +0100

    Renamed internal Trainer name

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SciLors Mashed Trainer")]
+[assembly: AssemblyTitle("SciLor's Mashed Trainer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SciLors Mashed Trainer")]
+[assembly: AssemblyProduct("SciLor's Mashed Trainer")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
