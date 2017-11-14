83e3832c537ca53344dd40dad134b8f3b9a77fbd
SciLor <scilor@users.noreply.github.com>
2017-11-14 20:16:04 +0100

    Implemented random weapon equip and prepared weaponboxes

--- Types/Settings/Game/GameSettings.cs
+++ Types/Settings/Game/GameSettings.cs
@@ -7,9 +7,13 @@ using System.Threading.Tasks;
 namespace SciLors_Mashed_Trainer.Types.Settings.Game {
     public class GameSettings {
         public DriveOverReviveSettings DriveOverReviveSettings { get; set; }
+        public RandomWeaponSettings RandomWeaponSettings { get; set; }
+        public WeaponBoxesSettings WeaponBoxesSettings { get; set; }
 
         public GameSettings() {
             DriveOverReviveSettings = new DriveOverReviveSettings();
+            RandomWeaponSettings = new RandomWeaponSettings();
+            WeaponBoxesSettings = new WeaponBoxesSettings();
         }
     }
 }
---
+++ Types/Settings/Game/WeaponBoxesSettings.cs
@@ -0,0 +1,19 @@
+﻿using SciLors_Mashed_Trainer.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SciLors_Mashed_Trainer.Types.Settings.Game {
+    public class WeaponBoxesSettings {
+        public bool IsEnabled { get; set; }
+        public UcWeaponSelector WeaponSelector;
+        public bool IsSkipBots { get; set; }
+
+        public WeaponBoxesSettings() {
+            IsEnabled = false;
+            IsSkipBots = true;
+        }
+    }
+}
